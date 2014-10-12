fcdaca5423990e6258352e96eed9aebb153b4ae0
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-12 20:26:14 +0530

    Yogesh || Temporary removing check

--- initialize-android.sh
+++ initialize-android.sh
@@ -3,9 +3,6 @@
 # raise an error if any command fails!
 set -e
 
-INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies"
-
-if [ ! -e ${INITIALIZATION_FILE} ]; then
   # fetch and initialize $ANDROID_HOME
   download-android
   # Use the latest android sdk tools
@@ -22,4 +19,3 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-19 --all > /dev/null
 
   touch ${INITIALIZATION_FILE}
-fi
