1aa5eac61a956f8c1d7601d7c68ef08b6414230a
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-13 13:37:41 +0530

    Yogesh || Correting folder to install npm dependencies

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 sh android-wait-for-emulator
 
-npm install mocha chai chai-as-promised colors underscore wd
 cd appium && node . &
+cd test && npm install mocha chai chai-as-promised colors underscore wd
 cd test && mocha android-simple.js
