e378e628fa1c3713af5eb5f7ea66f3cfb5f643c2
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-13 13:53:36 +0530

    Yogesh || correted way another script is invoked

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/appium/appium.git
 cd appium && ./reset.sh --android
-sh initialize-android.sh
+sh ./initialize-android.sh
 echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
-sh android-wait-for-emulator
+sh ./android-wait-for-emulator
 
 cd appium && node . &
 cd test && npm install mocha chai chai-as-promised colors underscore wd
