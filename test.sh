bfe3fdfafac7ce470c59b1f31e337550f8c32311
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-13 13:58:30 +0530

    Yogesh || trying with build steps

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
-git clone https://github.com/appium/appium.git
-cd appium && ./reset.sh --android
-sh ./initialize-android.sh
+./initialize-android.sh
 echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
-sh ./android-wait-for-emulator
+./android-wait-for-emulator
 
-cd appium && node . &
+git clone https://github.com/appium/appium.git
+cd appium && ./reset.sh --android && appium && node . &
 cd test && npm install mocha chai chai-as-promised colors underscore wd
 cd test && mocha android-simple.js
