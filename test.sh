9f57f3d95d2ecc62383a46cf05d2bbe3d2ec109d
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-15 22:11:27 +0530

    Yogesh || Not required to init android. Moving wait for emulator after other operation

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-./initialize-android.sh
+#./initialize-android.sh
 echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
-./android-wait-for-emulator
 
 git clone https://github.com/appium/appium.git
 (cd appium && git checkout -b v1.2.1 v1.2.1)
 (cd appium && ./reset.sh --verbose --android)
+
+./android-wait-for-emulator
